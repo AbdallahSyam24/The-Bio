2b8b1153600a711abe002a221dfe81c771be4187
Abdallah Syam <a.syam24@hotmail.com>
2025-10-13 11:25:35 -0700

    add url to the news data

--- api/controller/api.newsController.js
+++ api/controller/api.newsController.js
@@ -4,12 +4,12 @@ const news = mongoose.model("news");
 
 
 const insertNews = async (req, res) => {
-    const { title, body, type } = req.body;
-    if (!title || !body || !type) {
+    const { title, body, type, url } = req.body;
+    if (!title || !body || !type || !url) {
         return res.status(400).json({ error: 'All fields are required' });
     }
     try {
-        const newNews = new news({ title, body, type });
+        const newNews = new news({ title, body, type, url });
         await newNews.save();
         return res.status(201).json({ message: 'News added successfully', data: newNews });
     } catch (err) {
--- services/news.model.js
+++ services/news.model.js
@@ -13,6 +13,10 @@ const newsSchema = mongoose.Schema({
         type: String,
         required: true
     },
+    "url": {
+        type: String,
+        required: true
+    },
     "date": {
         type: Date,
         default: Date.now
