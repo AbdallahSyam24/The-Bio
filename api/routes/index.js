e6e58810f2ae10b46db00c1c1f7ac9efcfcdfc07
Abdallah Syam <a.syam24@hotmail.com>
2025-10-13 12:21:24 -0700

    fix bugs

--- api/routes/index.js
+++ api/routes/index.js
@@ -20,21 +20,23 @@ routes.route("/getLatest/:engine")
         monitor.setEngine(engine);
         const data = await monitor.start();
 
-        const { title, type, url } = data[0];
+        const { title, type, url } = data[0] || {};
 
-        try {
-            const result = await getMemory(title, type, url);
-            if (result.result) {
-                return res.status(200).json({ message: 'No new updates', data: [] });
-            } else {
-                return res.status(200).json({ data });
+        if (data[0]) {
+            try {
+                const result = await getMemory(title, type, url);
+                if (result.result) {
+                    return res.status(200).json({ message: 'No new updates', data: [] });
+                } else {
+                    return res.status(200).json({ data });
+                }
+            } catch (err) {
+                return res.status(500).json({ error: 'Internal Server Error' });
             }
-        } catch (err) {
-            return res.status(500).json({ error: 'Internal Server Error' });
         }
     });
 
 routes.route("/addNews")
-.post(insertNews);
+    .post(insertNews);
 
 module.exports = routes;
